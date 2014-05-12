fd071faa3cbe9baea92f217bc98c618561c9ed3b
spencer and drake <spence+drake@appacademy.io>
2014-05-12 11:22:42 -0400

    add tile randomization and make_board

--- minesweeper.rb
+++ minesweeper.rb
@@ -10,16 +10,30 @@ end
 class Board
   
   def initialize(size)
-    
+    @board = make_board(size)
   end
   
+  def make_board(size)
+    
+    board = Array.new(size) do 
+      Array.new(size) do         
+        Tile.new
+      end
+    end
+    
+    board
+  end
   
 end
 
 class Tile
   def initialize(bomb_present = false)
-    @bomb_present = bomb_present
+    @bomb_present = random unless bomb_present
     @status = :covered
+  end
+  
+  def random
+    rand(3) == 0
   end
   
 end
