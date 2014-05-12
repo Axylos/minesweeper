99c916d2c9caa0a7ae2d19a2043a95d6738b5644
spencer and drake <spence+drake@appacademy.io>
2014-05-12 11:42:45 -0400

    Determine object statuses.

--- minesweeper.rb
+++ minesweeper.rb
@@ -21,34 +21,59 @@ class Board
       end
     end
     
+    board.determine_adjacencies
+    
     board
   end
   
   def print_board
     @board.each do |row|
-      row.each_index do |element|
+      row.map do |element|
         
         if element.status == :covered
-          "[ ]"
-        elsif element.status == :revealed
-        elsif
+          "[*]"
+        else
+          if element.status == :interior
+            "[_]"
+          elsif element.status == :flagged
+            "[F]"
+          else
+            "[#{element.num_adj}]"
+          end  
+        end
+      
           
       end
+      
+      puts
     end
   end
   
+  def determine_adjacencies
+    @board.
+  end
 end
 
 class Tile
   attr_reader :status
+  attr_accessor :num_adj
+  
   def initialize(bomb_present = false)
-    @bomb_present = random unless bomb_present
+    @bombed = random unless bomb_present
     @status = :covered
+    
   end
   
   def random
     rand(3) == 0
   end
+  
+
+    
+  
+  
+  
+  
   
 end
 
